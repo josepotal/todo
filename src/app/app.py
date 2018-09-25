02371289c8b9e3deb68ebf76f54bd5be1f2c4b9e
Josep Otal <j.otal.pares@gmail.com>
2018-09-24 20:30:57 -0500

    Add reaload to gunicorn and database docker to use volumes

--- src/app/app.py
+++ src/app/app.py
@@ -6,6 +6,14 @@ class Todos():
         pass
 
     def on_get(self, req, resp): # because of falcon
+        todo = {
+            "id": 1,
+            "complete": True,
+            "name": "Take out the trash",
+            "created": 1455151212,
+            "completed": 1455171642
+        }
+        print(todo)
         resp.status = falcon.HTTP_200
         resp.body = "Hello world"
         pass
